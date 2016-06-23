76996e2dd507f3c8a72b70bdcba25954937043c4
John Montefusco <montefusco.john@mgail.com>
2016-06-23 19:29:11 -0400

    adding vector implimentations for dot product

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,37 @@ struct Vector{
     z: f32,
 
 }
+// ImPLIMENTATION OF FUNCTIONS
+impl Vector {
+
+    // new fn, takes arguments // constuctors of the struct you are 
+    // implimenting for.. No Self
+    // convention is to call this new... 
+    // mainly just creates new version of "struct"
+    fn new(x:f32,y:f32,z:f32) -> Vector{
+        Vector{
+            x: x,
+            y: y,
+            z: z,
+        }
+        
+    }
+
+    fn dot(self,vec: Vector)-> f32{
+        
+        (self.x * vec.x) +            
+        (self.y * vec.y) +
+        (self.z * vec.z)
 
 
+    }
+
+    
+
+}
+
+
+// IMPLIMENTATIONS of Extrenal Traits
 
 //                 RHS        SELF ( type that we want to implemnt this op for)
 
@@ -52,18 +81,42 @@ impl std::ops::Add for Vector{
     }
 }
 
+impl std::ops::Sub for Vector{
+    type Output = Vector;
+    
+    fn sub(self,rhs:Vector) -> Vector{
+        Vector{
+        x: self.x - rhs.x,
+        y: self.y - rhs.y,
+        z: self.z - rhs.z,
+        }
+    }
+}
+
+
+
+
+
 
 
 fn main() {
     let vec1 = Vector{x: 1.2,y: 15.2,z: 225.3};
-    let vec2 = Vector{x: 2.2,y: 32.3,z: 33.33};
+    let vec2 = Vector::new(3.5,235.3,352.0);
     let float = 2.24;
 
+// print statements
     println!("Vecotr: {:?} + Vector : {:?} = {:?}", vec1,vec2,(vec1+vec2));
+    
+    println!("");
+    println!("Vecotr: {:?} - Vector : {:?} = {:?}", vec1,vec2,(vec1-vec2));
 
+    println!("");
     println!("Vecotr: {:?} * Float : {:?} = {:?}", vec1,float ,(vec1*float));
 
+    println!("");
     println!("Float: {:?} * Vector : {:?} = {:?}", float, vec1, (float * vec1));
-
-
+    
+    println!(",");
+    println!(" {:?} o {:?} = {:?}", vec1, vec2, vec1.dot(vec2));
+    // println!(" {:?} o {:?} = {:?}", vec1, vec2, Vector::dot(vec1, vec2));
 }
